dbee73dc70aab7803751f0187e5650101f09ce6b
SHADOW18695 <99009597+SHADOW18695@users.noreply.github.com>
2024-02-23 15:21:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from config import sudo_group, log_channel
 batch = []
 
 bot = Client(
-    "bot",
+    "LOVE",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
